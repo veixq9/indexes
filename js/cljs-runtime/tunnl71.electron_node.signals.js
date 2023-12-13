ef9defc82c395bacd93ce4257b8b5c32e7143edd
veix <veixq9@protonmail.com>
2023-12-13 19:50:15 +0100

    file moves

--- js/cljs-runtime/tunnl71.electron_node.signals.js
+++
@@ -1,7 +0,0 @@
-goog.provide('tunnl71.electron_node.signals');
-tunnl71.electron_node.signals.cos = (function tunnl71$electron_node$signals$cos(d){
-return cljs.core.cycle(cljs.core.map.cljs$core$IFn$_invoke$arity$2(cljs.math.cos,cljs.core.range.cljs$core$IFn$_invoke$arity$3((0),((2) * Math.PI),d)));
-});
-cljs.core.take.cljs$core$IFn$_invoke$arity$2((100),tunnl71.electron_node.signals.cos(0.2));
-
-//# sourceMappingURL=tunnl71.electron_node.signals.js.map
